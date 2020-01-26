beac9a7449ffeb24420ad43a293ab683368ce62d
geethapriya08 <y.geethapriya@gmail.com>
2020-01-26 12:12:34 +0530

    This is my Change Signed-off-by: geethapriya08 <y.geethapriya@gmail.com>

--- QA_automation/src/test/java/com/crm/testscripts/MyJavaPractise.java
+++ QA_automation/src/test/java/com/crm/testscripts/MyJavaPractise.java
@@ -2,6 +2,9 @@ package com.crm.testscripts;
 
 public class MyJavaPractise {
 
+	public void myFirstMethod() {
+		System.out.println("This is my first method");
+	}
 	public static void main(String[] args) {
 
 System.out.println("This is my java file");
