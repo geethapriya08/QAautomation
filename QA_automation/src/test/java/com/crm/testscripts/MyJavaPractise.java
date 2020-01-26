58308873d7899c74037a377dff81206971909bb8
geethapriya08 <y.geethapriya@gmail.com>
2020-01-26 12:16:51 +0530

    This is my changes Signed-off-by: geethapriya08 <y.geethapriya@gmail.com>

--- QA_automation/src/test/java/com/crm/testscripts/MyJavaPractise.java
+++ QA_automation/src/test/java/com/crm/testscripts/MyJavaPractise.java
@@ -2,6 +2,9 @@ package com.crm.testscripts;
 
 public class MyJavaPractise {
 
+	public void mySecondMethod() {
+		System.out.println("This is my second");
+	}
 	public static void main(String[] args) {
 
 System.out.println("This is my java file");
